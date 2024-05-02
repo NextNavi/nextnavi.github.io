141e741d9c1159205721bcc8405481a514635146
andy <andy.chu.cs@icloud.com>
2024-05-03 00:23:26 +0800

    first commit

---
+++ _next/static/Hnf-fCUPrZr39KX60RDnb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/198-7b738c7dc36033b2.js","static/chunks/pages/index-1a1787f881ce24c4.js"],"/_error":["static/chunks/pages/_error-76826f8ed8f41aa0.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
