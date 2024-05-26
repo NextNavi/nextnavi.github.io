eb7f225663cacc18b17b9b36cdfec2eaea7195fa
andy <andy.chu.cs@icloud.com>
2024-05-26 20:27:05 +0800

    update

---
+++ _next/static/fbuWSa77o1GksAVjBUqhr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/228-8cc8e13fde21ef58.js","static/chunks/pages/index-04c9e31f4b4a1297.js"],"/_error":["static/chunks/pages/_error-76826f8ed8f41aa0.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
